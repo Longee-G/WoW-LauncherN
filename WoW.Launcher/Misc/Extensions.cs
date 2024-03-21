69f5916037e5e6c7821933cdf53eb1cc55ad8a19
longee-gao <longee@163.com>
2024-03-21 10:41:16 +0800

    temp commit

--- WoW.Launcher/Misc/Extensions.cs
+++ WoW.Launcher/Misc/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Arctium.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace WoW.Launcher.Misc;
 
 
@@ -14,5 +16,56 @@ static class Extensions
 		return copy;
 	}
 
+	public static short[] GetCopy(this short[] data)
+	{
+		var copy = new short[data.Length];
+		Array.Copy(data, copy, data.Length);
+		return copy;
+	}
 
+	// 找到数据中匹配pattern的位置
+	// 为什么pattern用short存储？ ... 原因是如果值是-1表示略过... 用byte就会空间不够
+	public static long FindPattern(this byte[] data, short[] pattern, long start, long baseOffset = 0)
+	{
+		long matches;
+		for(long i=start; i<data.Length; i++)
+		{
+			if (pattern.Length > (data.Length - i))
+				return 0;
+
+			for(matches = 0; matches < pattern.Length; matches ++)
+			{
+				if ((pattern[matches] != -1) && (data[i + matches] != (byte)pattern[matches]))
+					break;
+			}
+			if (matches == pattern.Length)
+				return baseOffset + i;
+		}
+
+		return 0;
+	}
+
+
+	public static long FindPattern(this byte[] data, short[] pattern, long baseOffset = 0) => FindPattern(data, pattern, 0L, baseOffset);
+
+	// 找到所有匹配的位置...
+	public static HashSet<long> FindPattern(this byte[] data, short[] pattern, int maxMatches, long maxOffset)
+	{
+		var matchList = new HashSet<long>();
+		long match = 0;
+
+		do
+		{
+			match = data.FindPattern(pattern, match, 0);
+			if (match == 0)
+				continue;
+
+			matchList.Add(match);
+			match += pattern.Length;
+		} while ((matchList.Count < maxMatches || match < maxOffset ) && match != 0 );
+
+		return matchList;
+	}
+
+	public static short[] ToPattern(this string data) => Encoding.UTF8.GetBytes(data).Select(b => (short)b).ToArray();
 }
--- WoW.Launcher/Misc/NativeWindows.cs
+++ WoW.Launcher/Misc/NativeWindows.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Arctium.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 
 namespace WoW.Launcher.Misc;
 
